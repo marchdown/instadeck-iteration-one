d44d2a19750f768084b5328f5bf011dcecfc0432
marchdown <marchdown@gmail.com>
2016-02-09 20:39:01 +0300

    reveal.js in place and working properly

---
+++ static/.#main.js
@@ -0,0 +1,15 @@
+// Full list of configuration options available at:
+// https://github.com/hakimel/reveal.js#configuration
+document.addEventListener("DOMContentLoaded", function(event)
+                          {
+                              Reveal.initialize({
+                                  controls: true,
+                                  progress: true,
+                                  history: true,
+                                  center: true,
+
+                                  transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+                                  // Optional reveal.js plugins
+
+                              });});
---
+++ static/main.js
@@ -0,0 +1,18 @@
+// Full list of configuration options available at:
+// https://github.com/hakimel/reveal.js#configuration
+
+document.addEventListener("DOMContentLoaded", function(event)
+                          // make sure that DOM is all nicely built before initializing reveal.js
+                          
+                          {
+                              Reveal.initialize({
+                                  controls: true,
+                                  progress: true,
+                                  history: true,
+                                  center: true,
+                                  theme: 'black',
+                                  transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+                                  // Optional reveal.js plugins
+
+                              });});
